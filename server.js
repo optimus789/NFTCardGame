f0287395b2030a76c1aacbe0a4e400c0a9d3dbdb
optimus789 <vinaysudrik@gmail.com>
2022-01-15 13:07:34 +0530

    changes for server.js

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ const server = app.listen(port, () => {
   console.log(`server is listening on Port  ${port}`);
 });
 
-const trackRouter = require('./routes/track');
+// const trackRouter = require('./routes/track');
 
-var testAPIRouter = require('./routes/api_status');
-const nftRouter = require('./routes/nftRoutes');
+// var testAPIRouter = require('./routes/api_status');
+// const nftRouter = require('./routes/nftRoutes');
 
 app.use('/track', trackRouter);
 app.use('/nft', nftRouter);
@@ -53,48 +53,6 @@ app.get('/profile', (req, res) => {
   res.render('profile');
 });
 
-// app.get("/:username", (req, res) => {
-
-//   loadArtist(req.params.username,res);
-//   res.send("Your name is "+ req.params.username + "\n");
-// });
-
-// app.get("/:username/:track", (req, res) => {
-
-//   loadTrack(req.params.username, req.params.track,res)
-
-// });
-
-// async function main() {
-//   const args = minimist(process.argv.slice(2));
-//   const token = args.token;
-
-//   if (!token) {
-//     return console.error(
-//       'A token is needed. You can create one on https://web3.storage',
-//     );
-//   }
-
-//   if (args._.length < 1) {
-//     return console.error(
-//       'Please supply the path to a file or directory',
-//     );
-//   }
-
-//   const storage = new Web3Storage({ token });
-//   const files = [];
-
-//   for (const path of args._) {
-//     const pathFiles = await getFilesFromPath(path);
-//     files.push(...pathFiles);
-//   }
-
-//   //console.log(`Uploading ${files.length} files`);
-//   const cid = await storage.put(files);
-//   //console.log('Content added with CID:', cid);
-// }
-
-//FOR VIDEO SECTION
 const router = express.Router();
 const axios = require('axios');
 
